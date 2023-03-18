8bc719583a91483b80a590b61d2775d1415a4c02
FrederickKisife <kilafrederick@gmaul.com>
2023-03-18 11:40:17 -0700

    Learning how to do css in react native

--- App.js
+++ App.js
@@ -4,8 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Fred Welcome to react native</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello, Fred</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.boldText}>Lorem ipsum <Text>Test</Text> dolor sit amet</Text>
+        <Text>Lorem ipsum dolor sit amet</Text>
+        <Text>Lorem ipsum dolor sit amet</Text>
+      </View>
     </View>
   );
 }
@@ -17,5 +23,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     
+  },
+  header:{
+    backgroundColor:'pink',
+    padding: 20,
+  },
+  boldText:{
+    fontWeight: 'bold',
+  }, body:{
+    backgroundColor: 'yellow',
+    padding: 20,
   },
 });
