42d4198c816f95dc83e6f1bee3da1125d8bc0a42
FrederickKisife <kilafrederick@gmaul.com>
2023-03-18 15:44:05 -0700

    styling the todos

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/header';
+import TodoItem from './components/todoItem';
 
 
 export default function App() {
@@ -20,7 +21,7 @@ export default function App() {
         <FlatList
           data={todos}
           renderItem={({ item }) =>(
-            <Text>{item.text}</Text>
+            <TodoItem item={item}/>
           )}
         />
       </View>
