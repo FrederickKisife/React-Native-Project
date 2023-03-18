df879cdcd60955ac11c61485da038be7b80b94f3
FrederickKisife <kilafrederick@gmaul.com>
2023-03-18 13:50:28 -0700

    touchable componenets

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
@@ -12,13 +12,25 @@ export default function App() {
     { name: 'Ketch', id: '7' },
   ])
 
+  const pressHandler = (id)=>{
+    console.log(id);
+    setPeople((prevPerson)=>{
+      return prevPerson.filter((person => person.id != id))
+    })
+  }
+
   return (
     <View style={styles.container}>
       
     <FlatList
+      numColumns={2}
+      keyExtractor={(item) => item.id}
       data={people}
       renderItem={({ item })=>(
-        <Text style={styles.item}>{item.name}</Text>
+        <TouchableOpacity onPress={()=>pressHandler(item.id)}>
+          <Text style={styles.item}>{item.name}</Text>
+        </TouchableOpacity>
+        
       )}
     />
 
@@ -50,5 +62,7 @@ const styles = StyleSheet.create({
     padding: 30,
     backgroundColor: 'pink',
     fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24,
   }
 });
