47548c6d330b3adb876cb07453da4650db952ed0
FrederickKisife <kilafrederick@gmaul.com>
2023-03-18 19:59:00 -0700

    finised

--- components/addTodo.js
+++ components/addTodo.js
@@ -1,31 +1,37 @@
-import React, {useState} from "react";
-import { StyleSheet, Text, View, TextInput, Button} from "react-native";
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
 
-export default function AddTodo({submitHandler}){
-    const [text, setText] = useState('')
+export default function AddTodo({ submitHandler }) {
+  [text, setText] = useState('');
 
-    const changeHandler= (val)=>{
-        setText(val)
-    }
+  const changeHandler = (val) => {
+    setText(val);
+  };
 
-    return(
-        <View>
-            <TextInput
-                style = {styles.input}
-                placeholder ="new todo..."
-                onChangeText={changeHandler}
-            />
-            <Button onPress={()=> submitHandler(text)} title= 'add todo' color='coral'/>
-        </View>
-    )
+  const pressHandler = () => {
+    submitHandler(text);
+    setText('');
+  }
+
+  return (
+    <View>
+      <TextInput 
+        style={styles.input} 
+        placeholder='new todo...'
+        onChangeText={changeHandler} 
+        value={text} 
+      />
+      <Button color='coral' onPress={pressHandler} title='add todo' />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    input:{
-        marginBottom: 10,
-        paddingHorizontal: 8,
-        paddingVertical: 6,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ddd'
-    }
-})
+  input: {
+    marginBottom: 10,
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+});
--- components/todoItem.js
+++ components/todoItem.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import {StyleSheet, TouchableOpacity, Text} from 'react-native';
+import {StyleSheet, TouchableOpacity, Text, View} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
-export default function TodoItem({  item, pressHandler }) {
+export default function TodoItem({ pressHandler, item }) {
   return (
-    <TouchableOpacity onPress={()=> pressHandler(item.key)}>
-      <Text style={styles.item}>{item.text}</Text>
+    <TouchableOpacity onPress={() => pressHandler(item.key)}>
+      <View style={styles.item}>
+        <MaterialIcons name='delete' size={18} color='#333' />
+        <Text style={styles.itemText}>{item.text}</Text>
+      </View>
     </TouchableOpacity>
   )
 }
+
 
 const styles = StyleSheet.create({
   item: {
@@ -18,5 +23,10 @@ const styles = StyleSheet.create({
     borderStyle: "dashed",
     borderRadius: 1,
     borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  itemText: {
+    marginLeft: 10,
   }
 });
